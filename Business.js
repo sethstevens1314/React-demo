599c4a04336a6d85184ed9a326b55e4b03524017
DevDude <68089783+sethstevens1314@users.noreply.github.com>
2020-08-15 16:25:45 -0400

    Second Upload of files

---
+++ Business.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Business.css';
+
+
+
+
+
+class Business extends React.Component{
+    render(){
+      const divStyle = {
+        backgroundImage: 'url('+ this.props.business.imageSrc + ')'
+      };
+      
+        return (
+        <div className="Business"style={divStyle}>
+        <div className="transbox">
+          <h2>{this.props.business.name}</h2>
+        <div className="Business-information">
+          <div className="Business-address">
+            <p>{this.props.business.address}</p>
+            <p>{this.props.business.city}</p>
+            <p>{this.props.business.state} {this.props.business.zipCode}</p>
+          </div>
+          <div className="Business-reviews">
+            <h3>{this.props.business.category.toUpperCase()}</h3>
+            <h3 className="rating">{`${this.props.business.rating} stars`}</h3>
+            <p>{this.props.business.reviewCount} reviews </p>
+            </div>
+          </div>
+        </div>
+      </div>);
+    }
+}
+export default Business;
